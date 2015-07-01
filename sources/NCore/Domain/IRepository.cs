d469491f5ab7f661c76c4f986e9f614204ad2303
Sergey Ivanov <s.ivanov@integriks.ru>
2015-07-01 14:49:21 +0300

    Добавление саммари к репозиторию и транзакции.

--- sources/NCore/Domain/IRepository.cs
+++ sources/NCore/Domain/IRepository.cs
@@ -6,13 +6,41 @@ namespace NCore.Domain
     {
     }
 
+    /// <summary>
+    /// Репозиторий
+    /// </summary>
     public interface IRepository<TEntity, TPrimaryKey> : IRepository where TEntity : Entity<TPrimaryKey>
     {
+        /// <summary>
+        /// Получение всех элементов репозитория
+        /// </summary>
         IQueryable<TEntity> GetAll();
+
+        /// <summary>
+        /// Получение элемента репозитория по ключу
+        /// </summary>
         TEntity Get(TPrimaryKey key);
+
+        /// <summary>
+        /// Добавление элемента в репозиторий
+        /// </summary>
         void Insert(TEntity entity);
+
+        /// <summary>
+        /// Обновление элемента в репозитории
+        /// </summary>
         void Update(TEntity entity);
+
+        /// <summary>
+        /// Удаление элемента из репозитория по ключу
+        /// </summary>
+        /// <param name="id"></param>
         void Delete(TPrimaryKey id);
+
+        /// <summary>
+        /// Удаление элемента из репозитория
+        /// </summary>
+        /// <param name="entity"></param>
         void Delete(TEntity entity);
     }
 }
--- sources/NCore/Domain/ITransaction.cs
+++ sources/NCore/Domain/ITransaction.cs
@@ -2,10 +2,19 @@
 
 namespace NCore.Domain
 {
+    /// <summary>
+    /// Транзакция
+    /// </summary>
     public interface ITransaction : IDisposable
     {
+        /// <summary>
+        /// Фиксация изменений
+        /// </summary>
         void Commit();
 
+        /// <summary>
+        /// Откат изменений
+        /// </summary>
         void Rollback();
     }
 }
